b999619439c98ffab9292b0a782d883ffb254110
Pavlen <pavlen@192.168.1.84>
2023-05-20 15:34:50 -0700

    Docstrings Функций

--- homework.py
+++ homework.py
@@ -8,10 +8,10 @@ from telegram.ext import CommandHandler, Updater
 from dotenv import load_dotenv
 load_dotenv()
 
-
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+# secret_token = os.getenv('TOKEN')
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -26,22 +26,31 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
+    """Проверка доступности переменных окружения,
+    которые необходимы для работы программы."""
     ...
 
 
 def send_message(bot, message):
+    """Отправляет сообщение в Telegram чат, 
+    определяемый переменной окружения TELEGRAM_CHAT_ID."""
     ...
 
 
 def get_api_answer(timestamp):
+    """Делает запрос к единственному эндпоинту API-сервиса."""
     ...
 
 
 def check_response(response):
+    """Проверяет ответ API на соответствие документации
+    из урока API сервиса Практикум.Домашка."""
     ...
 
 
 def parse_status(homework):
+    """Извлекает из информации о конкретной 
+    домашней работе статус этой работы."""
     ...
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
